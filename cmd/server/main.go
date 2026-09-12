0b013359679a69581acadd0b2138d08ad0103a9f
estebanc <esteban.oz220106@gmail.com>
2026-09-12 02:18:43 -0600

    chore: primer commit

---
+++ cmd/server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{
+		AppName: "Logistics Fintech API v1.0",
+	})
+
+	app.Use(recover.New())
+	app.Use(logger.New())
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"status":  "online",
+			"message": "Engine is running smoothly",
+		})
+	})
+
+	log.Fatal(app.Listen(":8080"))
+}
